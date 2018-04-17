fc416c4fdb5ff3c6706a2e8d2ca8e2eeeb95f681
MacBookDesarrolloTecno01 <macbookdesarrollotecno01@MacBookDesarrolloTecno02.local>
2018-04-17 17:02:00 -0600

    calculos, validaciones, ajuste de letra

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -11,22 +11,31 @@ import UIKit
 class ViewController: UIViewController {
 
     var operacionValor: String = ""
+    var ultimoDigitoNumero: Bool = false
     @IBOutlet weak var Operacion: UILabel!
     @IBOutlet weak var Resultado: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.Operacion.minimumScaleFactor = 0.5
+        self.Resultado.minimumScaleFactor = 0.5
+        self.Operacion.adjustsFontSizeToFitWidth = true
+        self.Resultado.adjustsFontSizeToFitWidth = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    func concatenar(digito: String){
+    
+    func concatenar(digito: String, numero: Bool){
         self.operacionValor = self.Operacion.text!
-        self.Operacion.text = self.operacionValor + digito
+        if !(self.operacionValor == "" && numero == false) {
+            if self.ultimoDigitoNumero == false && numero == false {
+                self.operacionValor = String(self.operacionValor.prefix(self.operacionValor.count - 1))
+            }
+            self.Operacion.text = self.operacionValor + digito
+        }
+        self.ultimoDigitoNumero = numero
     }
     
     @IBAction func BorrarTodo(_ sender: UIButton) {
@@ -35,13 +44,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func AccionarNumero(_ sender: UIButton) {
-        self.concatenar(digito:String(sender.tag))
+        self.concatenar(digito:String(sender.tag), numero: true)
     }
     
     @IBAction func AccionarOperacion(_ sender: UIButton) {
+        var signoOperacion: String
+        switch sender.tag {
+        case 10:
+            signoOperacion = "÷"
+        case 11:
+            signoOperacion = "x"
+        case 12:
+            signoOperacion = "-"
+        case 13:
+            signoOperacion = "+"
+        default:
+            signoOperacion = ""
+        }
+        self.concatenar(digito:signoOperacion, numero: false)
     }
     
     @IBAction func Calcular(_ sender: UIButton) {
+        var resultadoFinal: String = self.Operacion.text!
+        resultadoFinal = resultadoFinal.replacingOccurrences(of: "x", with: "*").replacingOccurrences(of: "÷", with: "/")
+        let exp: NSExpression = NSExpression(format: resultadoFinal)
+        let result: Double = exp.expressionValue(with: nil, context: nil) as! Double
+        self.Resultado.text = String(result)
     }
     
 }
